09c4eb6131b95e19d0370abec5b9c5c84d2363f9
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2022-06-29 21:20:26 +0900

    目標の追加ウィンドウ作成

--- src/components/AddTargetBox.jsx
+++ src/components/AddTargetBox.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
+import { TextField, Button } from "@mui/material";
+import "./components_CSS/AddTargetBox.css";
 
 const AddTargetBox = (props) => {
   const { setAddTargetStatus } = props;
+  const [targetAmount, setTargetAmount] = useState("");
 
   const closeAddTargetStatus = () => {
     setAddTargetStatus(false);
+  };
+
+  const changeAmount = (e) => {
+    let tempNum = String(e.target.value).replace(/,/g, "");
+    if (isNaN(tempNum)) {
+      return;
+    }
+    setTargetAmount(Number(tempNum).toLocaleString());
   };
 
   return (
@@ -17,6 +28,35 @@ const AddTargetBox = (props) => {
           className="close-button"
         />
         <h3 className="modal-title">目標を追加</h3>
+
+        {/* 目標名称 */}
+        <div className="input-name-box">
+          <span className="input-span">名称</span>
+          <TextField id="standard-basic" variant="standard" fullWidth={true} />
+        </div>
+
+        {/* 目標金額 */}
+        <div className="amount-group">
+          <span className="input-span">金額</span>
+          <div className="target-amount">
+            <TextField
+              id="standard-basic"
+              variant="standard"
+              fullWidth={true}
+              inputProps={{
+                style: { textAlign: "right", paddingRight: "5px" },
+              }}
+              value={targetAmount}
+              onChange={changeAmount}
+            />
+            <span className="input-span">円</span>
+          </div>
+        </div>
+
+        {/* 登録ボタン */}
+        <Button onClick={closeAddTargetStatus} variant="contained">
+          登録
+        </Button>
       </div>
     </>
   );
